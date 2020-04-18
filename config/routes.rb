ae5688f6bd9a1f4c30830de3cb9d7e3402795d79
Gus <gustavo.schiavon@gmail.com>
2020-04-18 16:15:36 +0800

    Change routes for authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show] #altering path, lesson 65
   get 'jekyll-projects', to: 'portfolios#jekyll'
   get 'sass-projects', to: 'portfolios#sass'
