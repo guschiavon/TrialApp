6300c3a2fd0ca1ed24929fd85081966959814c3b
Gus <gustavo.schiavon@gmail.com>
2020-04-18 17:29:30 +0800

    Completed authentication with virtual attributes for user mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Only allows submissions that have a name attribute
+  validates_presence_of :name
+
+  # 'Self' is the user; 'split' cuts the string
+    def first_name
+      self.name.split.first
+    end
+    
+    def last_name
+      self.name.split.last
+    end
+
 end
